06243a6aa7966b3cf10329b18b8c13ab6ed52772
AndrewCS149 <Andrewcs149@gmail.com>
2020-04-30 23:23:52 -0700

    Complete jewelsAndStones771.js

--- javascript/easy/jewelsAndStones771.js
+++ javascript/easy/jewelsAndStones771.js
@@ -9,3 +9,17 @@ have.  You want to know how many of the stones you have are also jewels.
 The letters in J are guaranteed distinct, and all characters in J and S are
 letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
 */
+
+var numJewelIsInStones = function (J, S) {
+
+  var jewels = 0;
+
+  for (var i = 0; i < 50; i++) {
+    if (J.includes(S[i])) {
+      jewels++;
+    }
+  }
+  return jewels;
+}
+
+console.log(numJewelIsInStones('aA', 'aAAbbbb'));
