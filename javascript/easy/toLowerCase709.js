5a9b5b77f9b243390ba36f882dba95ff16cb7d05
AndrewCS149 <Andrewcs149@gmail.com>
2020-04-30 22:56:26 -0700

    Restruture files and complete toLowerCase709.js

---
+++ javascript/easy/toLowerCase709.js
@@ -0,0 +1,12 @@
+/*
+709. To Lower Case 
+Implement function ToLowerCase() that has a string parameter str, and 
+returns the same string in lowercase.
+Difficulty - Easy
+*/
+
+var toLowerCase = function (str) {
+  return str.toLowerCase();
+}
+
+console.log(toLowerCase('HELLO WORLD'));
--- javascript/toLowerCase709.js
+++
@@ -1,3 +0,0 @@
-// 709. To Lower Case 
-// Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
-// Difficulty - Easy
